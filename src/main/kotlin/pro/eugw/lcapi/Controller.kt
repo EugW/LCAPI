c3d187375a9171afbe5c1d2ac2ee0d0be15afd8b
EugW <wug.bond@gmail.com>
2018-04-15 18:45:44 +0500

    First stable release

--- src/main/kotlin/pro/eugw/lcapi/Controller.kt
+++ src/main/kotlin/pro/eugw/lcapi/Controller.kt
@@ -1,6 +1,5 @@
 package pro.eugw.lcapi
 
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -10,53 +9,33 @@ import java.io.File
 @RestController
 class Controller {
 
-    @GetMapping("/schools")
-    fun schools(@RequestParam(value = "lang", defaultValue = "en") lang: String): AvailableSchools {
-        val arrayList = ArrayList<CommonSchool>()
-        File("classes").listFiles().forEach {
-                val file = File(it, "school-info.json")
-                arrayList.add(CommonSchool(it.name, with(ObjectMapper().readTree(file)) {
-                    if (this.has(lang))
-                        this[lang].asText()
-                    else
-                        this["en"].asText()
-
-                }))
-        }
-        return AvailableSchools(arrayList)
-    }
-
     @GetMapping("/classes")
-    fun classes(@RequestParam school_id: String, @RequestParam(value = "lang", defaultValue = "en") lang: String): AvailableClasses {
+    fun classes(@RequestParam(value = "lang", defaultValue = "en") lang: String): AvailableClasses {
         val arrayList = ArrayList<CommonClass>()
-        val file = File("classes", school_id)
-        if (!file.exists())
-            return AvailableClasses(arrayList, "")
-        var schoolName = ""
-        file.listFiles().forEach {
-            if (it.name != "school-info.json") {
+        val dirs = File("classes")
+        dirs.listFiles().forEach {
+            val schoolId = it.name
+            val schoolName = with(ObjectMapper().readTree(File(File("info", schoolId), "school-info.json"))) {
+                if (this.has(lang))
+                    this[lang].asText()
+                else
+                    this["en"].asText()
+            }
+            it.listFiles().forEach {
                 val string = it.name.split(".")
-                arrayList.add(CommonClass(string[0], string[1], string[2]))
-            } else {
-                schoolName = with(ObjectMapper().readTree(it)) {
-                    if (this.has(lang))
-                        this[lang].asText()
-                    else
-                        this["en"].asText()
-
-                }
+                arrayList.add(CommonClass(string[0], string[1], string[2], schoolId, schoolName))
             }
         }
-        return AvailableClasses(arrayList, schoolName)
+        return AvailableClasses(arrayList)
     }
 
     @GetMapping("/class")
-    fun bells(@RequestParam school_id: String, @RequestParam clazz: String): JsonNode {
-        val file = File("classes", "$school_id/$clazz")
-        println(file.absolutePath)
-        if (!file.exists())
-            return ObjectMapper().createObjectNode()
-        return ObjectMapper().readTree(file)
+    fun clazz(@RequestParam school_id: String, @RequestParam clazz: String): Class {
+        val schedule = File("classes", "$school_id/$clazz.json")
+        val bells = File(File("info", school_id), "bells.json")
+        if (!schedule.exists() || !bells.exists())
+            return Class(ObjectMapper().createObjectNode(), ObjectMapper().createObjectNode())
+        return Class(ObjectMapper().readTree(schedule), ObjectMapper().readTree(bells))
     }
 
 }
--- src/main/kotlin/pro/eugw/lcapi/Results.kt
+++ src/main/kotlin/pro/eugw/lcapi/Results.kt
@@ -1,7 +1,7 @@
 package pro.eugw.lcapi
 
-data class AvailableClasses(val classes: ArrayList<CommonClass>, val school_name: String)
-data class CommonClass(val number: String, val letter: String, val subgroup: String)
-data class AvailableSchools(val schools: ArrayList<CommonSchool>)
-data class CommonSchool(val id: String, val name: String)
-data class Class(val schedule: String, val bells: String)
+import com.fasterxml.jackson.databind.JsonNode
+
+data class AvailableClasses(val classes: ArrayList<CommonClass>)
+data class CommonClass(val number: String, val letter: String, val subgroup: String, val school_id: String, val school_name: String)
+data class Class(val schedule: JsonNode, val bells: JsonNode)
